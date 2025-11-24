daec9b5507efd02ad2c8e96bf9a3dea9af322ff1
gojkovicstrahinja <gojkovic.strahinja@gmail.com>
2025-11-24 01:01:49 +0100

    Nov pocetak

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Uvezite React plugin
-
-export default defineConfig({
-  base: './', 
-  plugins: [react()], // Dodajte React plugin
-})
